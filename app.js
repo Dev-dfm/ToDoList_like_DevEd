8ec97ed28f7aa4db1194f7d4451a050a57e8e322
Dev-dfm <d.floresmedina@gmail.com>
2021-05-02 12:06:26 +0200

    Add JS ToDo function

---
+++ app.js
@@ -0,0 +1,33 @@
+// Selectors
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+
+//Event Listeners
+todoButton.addEventListener("click", addTodo);
+
+//Functions
+function addTodo(event){
+    // Prevent form from submitting
+    event.preventDefault();
+    // Create DIV for ToDo Task
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo"); // Add a class = "todo" to the div
+    // Create LI
+    const newTodo = document.createElement("li");
+    newTodo.classList.add("todo-item");
+    newTodo.innerText = "Task1";
+    todoDiv.appendChild(newTodo); // Define newTodo as child of todoDiv
+    // Check mark button
+    const completedButton = document.createElement("button");
+    completedButton.classList.add("complete-btn");
+    completedButton.innerHTML = "<i class="fas fa-check"></i>";
+    todoDiv.appendChild(completedButton);
+    // Check trash button
+    const trashButton = document.createElement("button");
+    trashButton.classList.add("delete-btn");
+    trashButton.innerHTML = "<i class="fas fa-trash"></i>";
+    todoDiv.appendChild(trashButton);
+    // Append to list
+    todoList.appendChild(todoDiv);
+}
