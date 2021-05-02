03c7f301f8bddf4e639ae5554f3748d468e00b20
Dev-dfm <d.floresmedina@gmail.com>
2021-05-02 13:43:20 +0200

    Add checkmark as completed and line-through

--- app.js
+++ app.js
@@ -42,6 +42,9 @@ function deleteCheck (e) {
         const todo = item.parentElement;
         todo.remove();
     }
-
-
+    // Check mark
+    if (item.classList[0] === "complete-btn") {
+        const todo = item.parentElement;
+        todo.classList.toggle("completed");
+    }
 }
